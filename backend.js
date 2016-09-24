a443e20ae71a2cda8d461e4dc0cfb7a1325d5580
Mitch Lillie <mitch@mitchlillie.com>
2016-09-23 17:32:23 -0700

    add more sources and make more robust

--- backend.js
+++ backend.js
@@ -4,46 +4,65 @@ const request = require('request')
 const cheerio = require('cheerio')
 const echoMungeWeb = require('echomunge-web')
 const EchoMunge = require('echomunge')
+const url = require('url')
 let $list
 let $article
 let db
 
-request('http://www.nytimes.com/column/trilobites', function (err, res, body) {
+// let source = {
+//   id: 1,
+//   url: 'http://www.nytimes.com/column/trilobites',
+//   li: '.theme-summary',
+//   link: '.story-link',
+//   text: '.story-body-text',
+//   depth: 1
+// }
+
+// let source = {
+//   id: 1,
+//   url: 'http://abcnews.go.com/Weird',
+//   li: '.ffl_obj',
+//   link: '#h_default a',
+//   text: '.article-copy',
+//   depth: 1
+// }
+
+let source = {
+  id: 1,
+  url: 'http://www.upi.com/Odd_News/',
+  li: '.upi_item',
+  title: '',
+  link: 'a',
+  text: '#article',
+  depth: 1
+}
+
+request(source.url, function (err, res, body) {
   if (err) {
     throw new Error(err)
   }
   $list = cheerio.load(body)
-  $list('.theme-summary').each(function (i, e) {
+
+  $list(source.li).each(function (i, e) {
     let $this = $list(this)
-    let storyLink = $list('.story-link', $this).attr('href')
+    let storyLink = $list(source.link, $this).attr('href')
+    storyLink = url.parse(storyLink)
+    if (storyLink.host == null) {
+      storyLink = url.resolve(source.url, storyLink.path)
+    } else {
+      storyLink = storyLink.href
+    }
+    console.log('storyLink: ', storyLink)
     request(storyLink, function (err, res, body) {
       if (err) {
         throw new Error(err)
       }
       $article = cheerio.load(body)
       db = new EchoMunge()
-      db.recordText($article('.story-body-text').text())
+      db.recordText($article(source.text).not('embed, script').text())
       console.log('==========')
       let str = db.makeText({ maxLength: 500, terminate: true }) + ' ' + db.makeText({ maxLength: 200, terminate: true }) + ' ' + db.makeText({ maxLength: 600, terminate: true })
       console.log(str)
     })
-    // console.log('l: ', l)
-    // echoMungeWeb(l, function (err, db) {
-    //   if (err) throw err
-    //   for (var i = 0; i < 100; i++) {
-    //     console.log(db.makeText({ maxLength: 500, terminate: true }))
-    //   }
-    // })
-    // let title = $('.headline', $this).text()
-    // let txt = $('.summary', $this).text()
-    // summaryTool.summarize(title, txt, function (err, summary) {
-    //   if (err) {
-    //     throw new Error(err)
-    //   }
-    //   console.log('summary: ', summary)
-    //   console.log('Original Length ' + (title.length + txt.length))
-    //   console.log('Summary Length ' + summary.length)
-    //   console.log('Summary Ratio: ' + (100 - (100 * (summary.length / (title.length + txt.length)))))
-    // })
   })
 })
