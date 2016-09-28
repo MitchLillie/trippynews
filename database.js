54fa214496744cefcbf0d76627a9691b82036129
Mitch Lillie <mitch@mitchlillie.com>
2016-09-28 08:52:45 -0700

    pull db secrets into gitignored file

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require('mongodb')
+const fs = require('fs')
+const {user, password} = JSON.parse(fs.readFileSync('secret.json'))
 
 // Connection URL
-var url = 'mongodb://client:' + process.argv[2] + '@ds041586.mlab.com:41586/trippynews_articles'
+var url = 'mongodb://' + user + ':' + password + '@ds041586.mlab.com:41586/trippynews_articles'
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function (err, db) {
