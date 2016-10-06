9c42fabca1b44a0e740b3adf6fb8dc642a1180ef
Mitch Lillie <mitch@mitchlillie.com>
2016-10-06 13:53:25 -0700

    lots of work to get jsx actually actually working
    # thanks @joefraley

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var React = require('react')
-var DOM = React.DOM
-var {div, button, ul, li, nav} = DOM
+var Article = require('./article')
 
 // This is just a simple example of a component that can be rendered on both
 // the server and browser
@@ -11,7 +10,7 @@ module.exports = React.createClass({
   // was first rendered. We also want the button to be disabled until the
   // component has fully mounted on the DOM
   getInitialState: function () {
-    return {items: this.props.items, disabled: true}
+    return {arts: this.props.arts, disabled: true}
   },
 
   // Once the component has been mounted, we can enable the button
@@ -19,24 +18,40 @@ module.exports = React.createClass({
     this.setState({disabled: false})
   },
 
-  // Then we just update the state whenever its clicked by adding a new item to
-  // the list - but you could imagine this being updated with the results of
-  // AJAX calls, etc
-  handleClick: function () {
-    this.setState({
-      items: this.state.items.concat('Item ' + this.state.items.length)
-    })
-  },
-
   // For ease of illustration, we just use the React JS methods directly
   // (no JSX compilation needed)
   // Note that we allow the button to be disabled initially, and then enable it
   // when everything has loaded
   render: function () {
+    let articles = this.props.arts.map((article, i) => {
+      return <Article {...article} key={i + 1} />
+    })
     return (
       <div>
-        <button>Hello</button>
+        <nav className='navbar navbar-fixed-top navbar-light bg-faded'>
+          <div className='text-wrap'>
+            <svg viewBox='0 0 130 110' width='325px' height='85px'>
+              <pattern id='p-img' viewBox='0 0 290 200' patternUnits='userSpaceOnUse' width='300%' height='260%' x='-30%' y='-60%'>
+                <image href='flickr.com_SurFeRGiRL30_CC-BY-2.0.jpg' width={500} height={500} />
+              </pattern>
+              <text textAnchor='left' dy='1.15em' dx='-1.8em' className='img-layer'>Trippy News</text>
+            </svg>
+          </div>
+        </nav>
+        <div className='container articles'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-8'>
+                {articles}
+              </div>
+              <div className='col-md-3'>
+                <script src='//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US'></script>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
     )
   }
 })
@@ -48,59 +63,3 @@ module.exports = React.createClass({
 //     return li(null, item)
 //   })})
 // )
-
-// <nav class="navbar navbar-fixed-top navbar-light bg-faded">
-//   <div class="text-wrap">
-//     <svg viewBox="0 0 100 100" width="400px" height="100px">
-//       <pattern id="p-img" viewBox="0 0 300 200" patternUnits="userSpaceOnUse" width="300%" height="200%" x="100px" y="-10%">
-//         <image xlink:href="https://s-media-cache-ak0.pinimg.com/originals/93/e6/58/93e6585e1eb37e17ec9a7d3d55679147.jpg" width="300" height="300"></image>
-//       </pattern>
-//       <text text-anchor="left" dy="1.2em" dx="-1.8em" class="img-layer">Trippy News</text>
-//     </svg>
-//   </div>
-// </nav>
-// <div class="container articles">
-//   <div class="container">
-//     <div class="row">
-//       <div class="col-md-8">
-//         <div class="row">
-//           <div class="col-md-10 offset-md-2"><img src="https://www.quantamagazine.org/wp-content/uploads/2016/09/Dragonfly.jpg" class="img-fluid"/></div>
-//         </div>
-//         <div class="row">
-//           <div class="col-md-1 offset-md-1">
-//             <p>›</p>
-//           </div>
-//           <div class="col-md-10">
-//             <h3>I Fail to Scare Party Guests</h3>
-//             <p>The $30. We've reached out to advise the humor in Canada will no longer sell a Halloween decoration depicts a Halloween decoration depicting a window!</p>
-//           </div>
-//         </div>
-//         <div class="row">
-//           <div class="col-md-6 offset-md-2">
-//             <p> <small>Tue Mar 8, 2016</small></p>
-//           </div>
-//         </div>
-//         <div class="row">
-//           <div class="col-md-10 offset-md-2"><img src="https://www.quantamagazine.org/wp-content/uploads/2016/09/Ghissi_Altarpeiece_1K.jpg" class="img-fluid"/></div>
-//         </div>
-//         <div class="row">
-//           <div class="col-md-1 offset-md-1">
-//             <p>›</p>
-//           </div>
-//           <div class="col-md-10">
-//             <h3>Pranjal Borkotoky Gently Waved his Game</h3>
-//             <p><a>Pranjal Borkotoky gently waved his golf club at the birds continued to squawk loudly while keeping an eye on the ball. Pranjal Borkotoky shared video of himself swinging at the birds as he wrote?</a></p>
-//           </div>
-//         </div>
-//         <div class="row">
-//           <div class="col-md-6 offset-md-2">
-//             <p><small>Wed Mar 9, 2016</small></p>
-//           </div>
-//         </div>
-//       </div>
-//       <div class="col-md-3">
-//         <script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>
-//       </div>
-//     </div>
-//   </div>
-// </div>
---
+++ article.js
@@ -0,0 +1,27 @@
+var React = require('react')
+
+module.exports = React.createClass({
+  render: function () {
+    return (
+      <div className='article'>
+        <div className='row'>
+          <div className='col-md-10 offset-md-2'><img src={this.props.src} className='img-fluid' /></div>
+        </div>
+        <div className='row'>
+          <div className='col-md-1 offset-md-1'>
+            <p>&rsaquo;</p>
+          </div>
+          <div className='col-md-10'>
+            <h3>{this.props.text.split('.')[0]}</h3>
+            <p>{this.props.text.split('.').slice(1).join('.').slice(1)}</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-md-6 offset-md-2'>
+            <p> <small>{this.props.date}</small></p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
