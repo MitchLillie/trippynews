9715f0bfae6b0dcc262b89514ef1e006f41f1f84
Mitch Lillie <mitch@mitchlillie.com>
2016-10-07 21:30:04 -0700

    link to original article

--- article.js
+++ article.js
@@ -5,15 +5,17 @@ module.exports = React.createClass({
     return (
       <div className='article'>
         <div className='row'>
-          <div className='col-md-10 offset-md-2'><img src={this.props.src || 'http://loremflickr.com/600/400/' + this.props.text.split(/[.!?]/)[0].split(' ')[0]} className='img-fluid' /></div>
+          <div className='col-md-10 offset-md-2'><a href={this.props.href}><img src={this.props.src || 'http://loremflickr.com/600/400/' + this.props.text.split(/[.!?]/)[0].split(' ')[0]} className='img-fluid' /></a></div>
         </div>
         <div className='row'>
           <div className='col-md-1 offset-md-1'>
             <p>&rsaquo;</p>
           </div>
           <div className='col-md-10'>
-            <h3>{this.props.text.split(/[.!?]/)[0]}</h3>
-            <p>{this.props.text.split(/[.!?]/).slice(1).join('.').slice(1)}</p>
+            <a href={this.props.href}>
+              <h3>{this.props.text.split(/[.!?]/)[0]}</h3>
+              <p>{this.props.text.split(/[.!?]/).slice(1).join('.')}</p>
+            </a>
           </div>
         </div>
         <div className='row'>
