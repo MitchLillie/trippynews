2ea0d32ce25863197cd5ec26910d20987f8985ca
Mitch Lillie <mitch@mitchlillie.com>
2016-10-07 17:31:38 -0700

    pull any old image just in case we didn't find one in scrape

--- article.js
+++ article.js
@@ -5,7 +5,7 @@ module.exports = React.createClass({
     return (
       <div className='article'>
         <div className='row'>
-          <div className='col-md-10 offset-md-2'><img src={this.props.src} className='img-fluid' /></div>
+          <div className='col-md-10 offset-md-2'><img src={this.props.src || 'http://loremflickr.com/600/400/' + this.props.text.split(/[.!?]/)[0].split(' ')[0]} className='img-fluid' /></div>
         </div>
         <div className='row'>
           <div className='col-md-1 offset-md-1'>
